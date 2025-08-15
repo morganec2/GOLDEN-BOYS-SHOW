87a73816ca48b4da7da7386f682b5c03fcee4c24
Morgane <morgane.cobigo@florizontechnologies.com>
2025-08-15 09:59:26 +0200

    mise à jour des photos

--- src/pages/gallery/galleryList.js
+++ src/pages/gallery/galleryList.js
@@ -1,7 +1,27 @@
 export const galleryList = [
   {
-    image: require("../../assets/gallery/deshabiller.webp"),
-    title: "Dylan",
+    image: require("../../assets/boys/nellyboys.jpg"),
+    title: "Nelly",
+    link: "https://boom-event.com/dylan/",
+  },
+  {
+    image: require("../../assets/boys/nelly1.jpg"),
+    title: "Nelly",
+    link: "https://boom-event.com/dylan/",
+  },
+  {
+    image: require("../../assets/boys/nelly2.jpg"),
+    title: "Nelly",
+    link: "https://boom-event.com/dylan/",
+  },
+  {
+    image: require("../../assets/boys/drogo1.jpg"),
+    title: "Drogo",
+    link: "https://boom-event.com/dylan/",
+  },
+  {
+    image: require("../../assets/boys/drogo2.jpg"),
+    title: "Drogo",
     link: "https://boom-event.com/dylan/",
   },
 ];
